bd880e1c732cf51b855a317f375c23b9e8392ea4
Shishishi888 <Shishishi996@163.com>
2022-07-24 22:10:25 +0800

    分布式组件-SpringCloud Alibaba-Nacos注册中心

--- backend/OnlineMall/onlinemall-coupon/src/main/java/com/tjulab/onlinemall/coupon/OnlineMallCouponApplication.java
+++ backend/OnlineMall/onlinemall-coupon/src/main/java/com/tjulab/onlinemall/coupon/OnlineMallCouponApplication.java
@@ -2,8 +2,10 @@ package com.tjulab.onlinemall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OnlineMallCouponApplication {
 
     public static void main(String[] args) {
