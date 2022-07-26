106fe94cba2593a16c56581d0365fa7ba6999391
Shishishi888 <Shishishi996@163.com>
2022-07-27 00:13:26 +0800

    分布式组件-SpringCloud Alibaba-Gateway 创建&测试API网关

---
+++ backend/OnlineMall/onlinemall-gateway/src/main/java/com/tjulab/onlinemall/gateway/OnlineMallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.tjulab.onlinemall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class OnlineMallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMallGatewayApplication.class, args);
+    }
+
+}
---
+++ backend/OnlineMall/onlinemall-gateway/src/test/java/com/tjulab/onlinemall/gateway/OnlinemallGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tjulab.onlinemall.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlinemallGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
